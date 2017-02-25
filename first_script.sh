fd713e49e6c7c6a5ee01a46dbe8825b2530d6219
Kotaro Fujimaki <kfujimaki@dhcp-10-132-153-118.uawifi.arizona.edu>
2017-02-25 12:29:42 -0700

    Added help function

--- first_script.sh
+++ first_script.sh
@@ -1,3 +1,8 @@
+if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
+	echo "Usage: sh $0 input_file output_file"
+	exit
+fi
+
 # This will output the number of characters in a file
 head -5 $1 | sort | wc -c > $2
 
